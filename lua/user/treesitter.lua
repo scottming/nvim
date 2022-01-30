5b854e6aa56feea08a9dcf19caf9386f0b87581b
scottming <therealscottming@gmail.com>
2022-01-30 11:00:31 +0800

    Add surrond

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,23 +1,24 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml", "elixir" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-}
+configs.setup({
+	-- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "eex", "heex", "elixir", "lua" },
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "haskell" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml", "elixir" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+})
