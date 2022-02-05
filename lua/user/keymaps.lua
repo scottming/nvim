b6fefacba2cab2498cddac1d42bda8f0e69c5d30
scottming <therealscottming@gmail.com>
2022-02-05 16:31:00 +0800

    Modify keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -42,7 +42,7 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
