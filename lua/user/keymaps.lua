d997d92647606db78b86ba7c2d0d0fc886f78c4d
Scott Ming <TheRealScottMing@Gmail.com>
2022-02-09 20:01:43 +0800

    Fix buffer keymap error

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", "<cmd>BufferNext<CR>", opts)
+keymap("n", "<S-h>", "<cmd>BufferPrevious<CR>", opts)
 
 -- Toggle iex
 keymap("n", "<C-t>", "<cmd>lua _IEX_TOGGLE()<cr>", opts)
