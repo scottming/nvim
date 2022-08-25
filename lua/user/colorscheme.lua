47ef171a31f7887bd0907f4ca091b7bbdc1b9091
scottming <therealscottming@gmail.com>
2022-08-25 10:54:18 +0800

    Revert

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,12 @@
 vim.cmd([[
 try
-  colorscheme dracula_pro
+  colorscheme onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
 ]])
 
---[[ -- only for onedark ]]
---[[ require("onedark").setup({ style = "cool" }) ]]
---[[ require("onedark").load() ]]
+-- only for onedark
+require("onedark").setup({ style = "cool" })
+require("onedark").load()
