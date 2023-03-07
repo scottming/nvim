bd5afe445e628a77a2317c421ba1c7d61c47e0e3
scottming <therealscottming@gmail.com>
2023-03-07 12:55:51 +0800

    Just make the window cursor movement more like the vscode

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -32,7 +32,7 @@ function _G.set_terminal_keymaps()
 	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n>|<cmd>lua require('user.util').win_up()<cr>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
--- lua/user/util.lua
+++ lua/user/util.lua
@@ -11,3 +11,30 @@ R = function(name)
 	RELOAD(name)
 	return require(name)
 end
+
+local M = {}
+
+-- Just make the behavor of win_up and win_down more like Vscode
+M.win_up = function()
+	local last_focus = vim.g.last_focus_win_number
+	-- when there is a focus and the focus is not the current window
+	if last_focus and last_focus ~= vim.fn.winnr() then
+		-- id is dynamic, so we need to get it every time
+		local last_focus_win_id = vim.fn.win_getid(last_focus)
+		vim.fn.win_gotoid(last_focus_win_id)
+	else
+		vim.cmd("wincmd k")
+	end
+end
+
+M.win_down = function()
+	if vim.fn.winnr("$") ~= vim.fn.winnr() then
+		vim.g.last_focus_win_number = vim.fn.winnr()
+		vim.cmd("wincmd j")
+	else
+		-- when the last focus is in the bottom window
+		vim.cmd("wincmd j")
+	end
+end
+
+return M
