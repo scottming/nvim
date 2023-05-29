c10f27117466b885bb292e83e7e6bd4f3494a598
scottming <therealscottming@gmail.com>
2023-05-29 19:39:00 +0800

    Add noice

---
+++ lua/user/noice.lua
@@ -0,0 +1,37 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+	commit = "7e358170cea2314d9768b3f63616e8fad186195e",
+	opts = {
+		lsp = {
+			progress = {
+				enabled = false,
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
+			},
+		},
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			-- command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+	},
+}
