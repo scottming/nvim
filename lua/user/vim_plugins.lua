7243fd342ae1da5e8485c47f82f0fbfe1e339638
Scott Ming <therealscottming@gmail.com>
2023-12-03 15:42:24 +0800

    Use global projectionist_heuristics value

--- lua/user/vim_plugins.lua
+++ lua/user/vim_plugins.lua
@@ -6,7 +6,99 @@ return {
 	-- surround
 	{ "tpope/vim-surround", commit = "3d188ed2113431cf8dac77be61b842acb64433d9", event = "VeryLazy" },
 	-- projectionist better product code and test code
-	{ "tpope/vim-projectionist", commit = "e292c4e33b2c44074c47c06e8ce8b309fd8099bc", event = "VeryLazy" },
+	{
+		"tpope/vim-projectionist",
+		commit = "e292c4e33b2c44074c47c06e8ce8b309fd8099bc",
+		event = "VeryLazy",
+		config = function()
+			vim.g.projectionist_heuristics = {
+				["*"] = {
+					["lib/**/views/*_view.ex"] = {
+						type = "view",
+						alternate = "test/{dirname}/views/{basename}_view_test.exs",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}View do",
+							"  use {dirname|camelcase|capitalize}, :view",
+							"end",
+						},
+					},
+					["test/**/views/*_view_test.exs"] = {
+						alternate = "lib/{dirname}/views/{basename}_view.ex",
+						type = "test",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}ViewTest do",
+							"  alias {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}View",
+							"",
+							"  use ExUnit.Case, async: true",
+							"end",
+						},
+					},
+					["lib/**/controllers/*_controller.ex"] = {
+						type = "controller",
+						alternate = "test/{dirname}/controllers/{basename}_controller_test.exs",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Controller do",
+							"  use {dirname|camelcase|capitalize}, :controller",
+							"end",
+						},
+					},
+					["test/**/controllers/*_controller_test.exs"] = {
+						alternate = "lib/{dirname}/controllers/{basename}_controller.ex",
+						type = "test",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}ControllerTest do",
+							"  use {dirname|camelcase|capitalize}.ConnCase, async: true",
+							"end",
+						},
+					},
+					["lib/**/channels/*_channel.ex"] = {
+						type = "channel",
+						alternate = "test/{dirname}/channels/{basename}_channel_test.exs",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Channel do",
+							"  use {dirname|camelcase|capitalize}, :channel",
+							"end",
+						},
+					},
+					["test/**/channels/*_channel_test.exs"] = {
+						alternate = "lib/{dirname}/channels/{basename}_channel.ex",
+						type = "test",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}ChannelTest do",
+							"  alias {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Channel",
+							"",
+							"  use {dirname|camelcase|capitalize}.ChannelCase, async: true",
+							"end",
+						},
+					},
+					["test/**/features/*_test.exs"] = {
+						type = "feature",
+						template = {
+							"defmodule {dirname|camelcase|capitalize}.{basename|camelcase|capitalize}Test do",
+							"  use {dirname|camelcase|capitalize}.FeatureCase, async: true",
+							"end",
+						},
+					},
+					["lib/*.ex"] = {
+						alternate = "test/{}_test.exs",
+						type = "source",
+						template = { "defmodule {camelcase|capitalize|dot} do", "end" },
+					},
+					["test/*_test.exs"] = {
+						alternate = "lib/{}.ex",
+						type = "test",
+						template = {
+							"defmodule {camelcase|capitalize|dot}Test do",
+							"  alias {camelcase|capitalize|dot}",
+							"",
+							"  use ExUnit.Case, async: true",
+							"end",
+						},
+					},
+				},
+			}
+		end,
+	},
 	-- bdelete, <leader>c
 	{ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56", event = "VeryLazy" },
 	-- maximize the window by <leader>z
