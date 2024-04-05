71ee626c6a7fa874626bc9be32d3629e582f62ca
Scott Ming <therealscottming@gmail.com>
2024-04-05 19:54:55 +0800

    Lock the fzf-lua

--- lua/user/fzf_lua.lua
+++ lua/user/fzf_lua.lua
@@ -6,5 +6,7 @@ return {
 		-- calling `setup` is optional for customization
 		require("fzf-lua").setup({ "telescope" })
 	end,
+	-- commit = "f430c5b3b1d531cff8d011474461ea3090e932e2",
+  commit = "2ba4a51eb36febd864735b7b25ac2bd1d1f7cf08",
 	event = "VeryLazy",
 }
